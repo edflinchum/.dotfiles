c7d2eeb879115550df3f4a2f5fca7d432b0247ff
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-05-28 17:04:12 -0400

    Add 1password cli completions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,11 @@ eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    # 1password cli
+    if type -q op
+      op completion fish | source
+    end
+
     # Starship
     if type -q starship
       starship init fish | source
