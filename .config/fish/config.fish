636d2bb0403f7efaf17948c1738c9fc7dbc80f8a
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-05-30 16:10:55 -0400

    Set default editor to micro

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,31 +5,41 @@
 # -------------------------------------------------------
 
 # Homebrew
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-if test -d (brew --prefix)"/share/fish/completions"
-  set -g fish_complete_path (brew --prefix)/share/fish/completions $fish_complete_path
+if test -d "$HOMEBREW_PREFIX/share/fish/completions"
+    set -g fish_complete_path $HOMEBREW_PREFIX/share/fish/completions $fish_complete_path
 end
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-  set -g fish_complete_path (brew --prefix)/share/fish/vendor_completions.d $fish_complete_path
+if test -d "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
+    set -g fish_complete_path $HOMEBREW_PREFIX/share/fish/vendor_completions.d $fish_complete_path
 end
 
 # 1password cli
 if type -q op
-  op completion fish | source
-  if test -e ~/.config/op/plugins.sh
-      source ~/.config/op/plugins.sh
-  end
+    op completion fish | source
+    set -gx SSH_AUTH_SOCK ~/.1password/agent.sock
+    if test -e ~/.config/op/plugins.sh
+        source ~/.config/op/plugins.sh
+    end
+end
+
+# Micro
+if type -q micro
+    set -gx EDITOR $(which micro)
 end
 
 # -------------------------------------------------------
 
-if status is-interactive
 # Commands to run in interactive sessions can go here
+if status is-interactive
 
   # Starship
   if type -q starship
       starship init fish | source
       enable_transience
+  end
+
+  # Fastfetch
+  if type -q fastfetch
+      fastfetch
   end
 
 end
