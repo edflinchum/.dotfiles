71f501779f72a6f7c5080fab5dee4935cd64d1bc
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-05-31 14:09:39 -0400

    Change dot alias to abbr to fix completions

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,47 +1,48 @@
-abbr -a -g -- cd.. 'cd ..'
-abbr -a -g -- cls clear
-abbr -a -g -- cp 'cp -iv'
-abbr -a -g -- df 'df -hT'
-abbr -a -g -- dots 'dot status'
-abbr -a -g -- du 'du -ach | sort -h'
-abbr -a -g -- egrep 'egrep --color=auto'
-abbr -a -g -- fgrep 'fgrep --color=auto'
-abbr -a -g -- free 'free -mt'
-abbr -a -g -- gbc git_branch_cleanup
-abbr -a -g -- jctl 'journalctl -p 3 -xb'
-abbr -a -g -- k kubectl
-abbr -a -g -- kctx kubectx
-abbr -a -g -- kcu 'kubectl config unset current-context'
-abbr -a -g -- kns kubens
-abbr -a -g -- mkdir 'mkdir -pv'
-abbr -a -g -- mv 'mv -iv'
-abbr -a -g -- psa 'ps auxf'
-abbr -a -g -- rm 'rm -Iv'
-abbr -a -g -- spta 'starship toggle aws'
-abbr -a -g -- sptg 'starship toggle gcloud'
-abbr -a -g -- sptz 'starship toggle azure'
-abbr -a -g -- sr 'sudo reboot'
-abbr -a -g -- ssn 'sudo shutdown now'
-abbr -a -g -- t 'tail -f'
-abbr -a -g -- wget 'wget -c'
+abbr -a -- cd.. 'cd ..'
+abbr -a -- cls clear
+abbr -a -- cp 'cp -iv'
+abbr -a -- df 'df -hT'
+abbr -a -- dot 'git --git-dir="$HOME/.dotfiles" --work-tree="$HOME"'
+abbr -a -- dots 'git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" status'
+abbr -a -- du 'du -ach | sort -h'
+abbr -a -- egrep 'egrep --color=auto'
+abbr -a -- fgrep 'fgrep --color=auto'
+abbr -a -- free 'free -mt'
+abbr -a -- gbc git_branch_cleanup
+abbr -a -- jctl 'journalctl -p 3 -xb'
+abbr -a -- k kubectl
+abbr -a -- kctx kubectx
+abbr -a -- kcu 'kubectl config unset current-context'
+abbr -a -- kns kubens
+abbr -a -- mkdir 'mkdir -pv'
+abbr -a -- mv 'mv -iv'
+abbr -a -- psa 'ps auxf'
+abbr -a -- rm 'rm -Iv'
+abbr -a -- spta 'starship toggle aws'
+abbr -a -- sptg 'starship toggle gcloud'
+abbr -a -- sptz 'starship toggle azure'
+abbr -a -- sr 'sudo reboot'
+abbr -a -- ssn 'sudo shutdown now'
+abbr -a -- t 'tail -f'
+abbr -a -- wget 'wget -c'
 
 if type -q apt
-  abbr -a -g -- upall 'sudo apt update; and sudo apt upgrade; and brew upgrade'
-  abbr -a -g -- update 'sudo apt update; and sudo apt upgrade'
+  abbr -a -- upall 'sudo apt update; and sudo apt upgrade; and brew upgrade'
+  abbr -a -- update 'sudo apt update; and sudo apt upgrade'
 end
 if type -q nala
-  abbr -a -g -- upall 'sudo nala upgrade; and brew upgrade'
-  abbr -a -g -- update 'sudo nala upgrade'
+  abbr -a -- upall 'sudo nala upgrade; and brew upgrade'
+  abbr -a -- update 'sudo nala upgrade'
 end
 if type -q dnf
-  abbr -a -g -- upall 'sudo dnf upgrade; and brew upgrade'
-  abbr -a -g -- update 'sudo dnf upgrade'
+  abbr -a -- upall 'sudo dnf upgrade; and brew upgrade'
+  abbr -a -- update 'sudo dnf upgrade'
 end
 if type -q pacman
-  abbr -a -g -- upall 'sudo pacman -Syyu; and brew upgrade'
-  abbr -a -g -- update 'sudo pacman -Syyu'
+  abbr -a -- upall 'sudo pacman -Syyu; and brew upgrade'
+  abbr -a -- update 'sudo pacman -Syyu'
 end
 if type -q paru
-  abbr -a -g -- upall 'paru -Syu --noconfirm; and brew upgrade'
-  abbr -a -g -- update 'paru -Syu --noconfirm'
+  abbr -a -- upall 'paru -Syu --noconfirm; and brew upgrade'
+  abbr -a -- update 'paru -Syu --noconfirm'
 end
--- .config/fish/functions/dot.fish
+++
@@ -1,3 +0,0 @@
-function dot --wraps=git --description 'Git alias for .dotfiles bare repo with HOME work tree'
-  git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" $argv; 
-end
