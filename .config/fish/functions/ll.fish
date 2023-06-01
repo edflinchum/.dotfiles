385c9a717e6d3cf14c541214498d0eb48926aead
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-01 09:42:54 -0400

    Simplify completions

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps=ls --wraps=exa --description 'alias ll exa -aFghl --color-scale --group-directories-first --time-style=long-iso --icons'
+function ll --wraps=exa --description 'alias ll exa -aFghl --color-scale --group-directories-first --time-style=long-iso --icons'
   exa -aFghl --color-scale --group-directories-first --time-style=long-iso --icons $argv; 
 end
