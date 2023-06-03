c167a09347ea7b139ccc291a8b6a6b7210cfcc9f
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-03 10:47:45 -0400

    Fixing completions for git add

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,14 @@
 #
 # ~/.config/fish/config.fish
 #
-
 # -------------------------------------------------------
 
-# Homebrew
+# Homebrew completions
 if test -d "$HOMEBREW_PREFIX/share/fish/completions"
-    set -g fish_complete_path $fish_complete_path $HOMEBREW_PREFIX/share/fish/completions
+    set -a fish_complete_path $HOMEBREW_PREFIX/share/fish/completions
 end
 if test -d "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
-    set -g fish_complete_path $fish_complete_path $HOMEBREW_PREFIX/share/fish/vendor_completions.d
+    set -a fish_complete_path $HOMEBREW_PREFIX/share/fish/vendor_completions.d
 end
 
 # 1password cli
@@ -28,20 +27,20 @@ if type -q micro
     set -gx EDITOR $(which micro)
 end
 
-# -------------------------------------------------------
+# Workaround for git completions taking too long due to searching untracked files in entire home directory
+# This is in addition to the custom modifications made to the default git completions file
+complete git -n '__fish_git_using_command add' --force-files
 
+# -------------------------------------------------------
 # Commands to run in interactive sessions can go here
 if status is-interactive
-
   # Starship
   if type -q starship
       starship init fish | source
       enable_transience
   end
-
   # Fastfetch
   if type -q fastfetch
       fastfetch
   end
-
 end
