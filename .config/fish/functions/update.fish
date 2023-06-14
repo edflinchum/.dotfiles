ace02432733e61011dd6bb726cd5ee9f644f50c4
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-14 13:04:23 -0400

    Update function can now handle quotes in os-release ID

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,29 +1,29 @@
 function update --description "Package manager independent update"
-  set -l ID (grep -Es ^ID= /etc/os-release | sed -e 's/ID=//g')
+  set -l ID (grep -Es ^ID= /etc/os-release)
 
   switch $ID
 
-    case arch
+    case '*arch*'
       if type -q paru
         paru -Syu
       else
         sudo pacman -Syu
       end
 
-    case debian ubuntu
+    case '*debian*' '*ubuntu*'
       if type -q nala
         sudo nala upgrade
       else
         sudo apt update; and sudo apt upgrade
       end
 
-    case fedora ultramarine
+    case '*fedora*' '*ultramarine*'
       sudo dnf upgrade
 
-    case '*leap'
+    case '*leap*'
       sudo zypper update
 
-    case '*tumbleweed'
+    case '*tumbleweed*'
       sudo zypper dist-upgrade
 
     case '*'
