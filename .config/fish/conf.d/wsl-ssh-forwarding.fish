a46a8f4341378f73bf59213434cdf2e436de665d
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-17 10:58:50 -0400

    Improve wsl ssh forwarding scripts

--- .config/fish/conf.d/wsl-ssh-forwarding.fish
+++ .config/fish/conf.d/wsl-ssh-forwarding.fish
@@ -3,7 +3,7 @@ if string match -qr WSL $(uname -a)
     set -gx SSH_AUTH_SOCK $HOME/.ssh/agent.sock
     # need `ps -ww` to get non-truncated command for matching
     # use square brackets to generate a regex match for the process we want but that doesn't match the grep command running it!
-    set ALREADY_RUNNING $(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $status)
+    set ALREADY_RUNNING $(ps -auxww | grep -q '[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent'; echo $status)
     if test $ALREADY_RUNNING != "0"
         if test -S $SSH_AUTH_SOCK
             # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
@@ -13,6 +13,6 @@ if string match -qr WSL $(uname -a)
         echo "Starting SSH-Agent relay..."
         # setsid to force new session to keep running
         # set socat to listen on $SSH_AUTH_SOCK and forward to npiperelay which then forwards to openssh-ssh-agent on windows
-        eval (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+        setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork >/dev/null 2>&1 & disown
     end
 end
