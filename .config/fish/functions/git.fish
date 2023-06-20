6e5856c1042da1fa1d29406bbebcf9a9499e4f7a
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-19 21:16:46 -0400

    Allow git clone to bypass git wrapper function

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,6 +1,7 @@
 function git --description "Git wrapper for .dotfiles bare repo when in home folder unless in a different git work tree"
-  if string match -qr $HOME $PWD
-    and not command git rev-parse --is-inside-work-tree &> /dev/null
+  if test $argv[1] != clone
+  and string match -qr $HOME $PWD
+  and not command git rev-parse --is-inside-work-tree &> /dev/null
     command git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" $argv
   else
     command git $argv
