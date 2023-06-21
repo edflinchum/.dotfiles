f135940af32a92283caba68d5a765e8cf2bf732c
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-21 15:12:40 -0400

    Refactor bash configs

--- .config/personal/bashrc-personal.bash
+++ .config/personal/bashrc-personal.bash
@@ -2,14 +2,14 @@
 #                         BASH configuration                         #
 ######################################################################
 
-# Use micro as default editor
-if type micro &>/dev/null; then
-  export EDITOR=$(command -v micro)
-  export VISUAL=$EDITOR
-fi
-
 # Environment variables
 export LESSHISTFILE=-
+
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
 
 ######################################################################
 #                        Additional features                         #
@@ -42,14 +42,22 @@ if type brew &>/dev/null; then
   fi
 fi
 
+# Use micro as default editor
+if type micro &>/dev/null; then
+  export EDITOR=$(command -v micro)
+  export VISUAL=$EDITOR
+fi
+
 # Load starship prompt
-if type starship &>/dev/null; then 
+if type starship &>/dev/null; then
   eval "$(starship init bash)"
 fi
 
 # Print some system information when the shell is first started
 if type fastfetch &>/dev/null; then
   fastfetch
+elif type neofetch &>/dev/null; then
+  neofetch
 else
   echo $USER@$HOST  $(uname -srm)  $(command -v lsb_release &>/dev/null && lsb_release -rcs)
 fi
