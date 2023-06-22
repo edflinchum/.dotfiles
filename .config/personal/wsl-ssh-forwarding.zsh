23689ab9355eafa45aa99cb934facf43e5946ee8
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-21 21:14:21 -0400

    Add a delay if booting with systemd

--- .config/personal/wsl-ssh-forwarding.zsh
+++ .config/personal/wsl-ssh-forwarding.zsh
@@ -1,19 +1,14 @@
 # Configure ssh forwarding if using WSL
 if (( $+commands[socat] )) && [[ $(uname -a) =~ WSL ]]; then
     export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-    # need `ps -ww` to get non-truncated command for matching
-    # use square brackets to generate a regex match for the process we want but that doesn't match the grep command running it!
     ALREADY_RUNNING=$(ps -auxww | grep -q '[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent'; echo $?)
     if [[ $ALREADY_RUNNING != "0" ]]; then
         if [ -S $SSH_AUTH_SOCK ]; then
-            # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
             echo "removing previous socket..."
             rm $SSH_AUTH_SOCK
-            sleep 8
+            [[ $(ps -ch 1) =~ systemd ]] && sleep 8
         fi
         echo "Starting SSH-Agent relay..."
-        # setsid to force new session to keep running
-        # set socat to listen on $SSH_AUTH_SOCK and forward to npiperelay which then forwards to openssh-ssh-agent on windows
         (setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork &>/dev/null &)
     fi
 fi
