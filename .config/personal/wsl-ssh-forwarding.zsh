4d099cbde7934cd3ef66b159cd1f153709ec61e2
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-21 20:54:42 -0400

    Add a delay to prevent socat starting too quickly

--- .config/personal/wsl-ssh-forwarding.zsh
+++ .config/personal/wsl-ssh-forwarding.zsh
@@ -9,10 +9,11 @@ if (( $+commands[socat] )) && [[ $(uname -a) =~ WSL ]]; then
             # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
             echo "removing previous socket..."
             rm $SSH_AUTH_SOCK
+            sleep 8
         fi
         echo "Starting SSH-Agent relay..."
         # setsid to force new session to keep running
         # set socat to listen on $SSH_AUTH_SOCK and forward to npiperelay which then forwards to openssh-ssh-agent on windows
-        (setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork >/dev/null 2>&1 &)
+        (setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork &>/dev/null &)
     fi
 fi
