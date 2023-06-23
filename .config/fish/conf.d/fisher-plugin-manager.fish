f8b8ba0ca50788d7137fedfec9d7e67c23dd3eb7
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-23 09:52:37 -0400

    Generalize fish config location

--- .config/fish/conf.d/fisher-plugin-manager.fish
+++ .config/fish/conf.d/fisher-plugin-manager.fish
@@ -1,5 +1,5 @@
 # Install fisher plugin manager if it is missing
-set -l fisher_path $HOME/.config/fish/functions/fisher.fish
+set -l fisher_path $__fish_config_dir/functions/fisher.fish
 if not test -e $fisher_path
   curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish -o $fisher_path
 end
