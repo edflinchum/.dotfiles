d1260c139ba83edbaddd04703d8fc82838683a23
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-24 19:51:26 -0400

    Improve comments and formatting

--- .config/personal/functions-personal.zsh
+++ .config/personal/functions-personal.zsh
@@ -3,7 +3,7 @@
 ######################################################################
 
 # Git wrapper for .dotfiles bare repo when in home folder unless in a different git work tree
-function git() {
+git() {
     if [[ "$1" != clone && "$PWD" =~ "$HOME" && ! $(command git rev-parse --is-inside-work-tree) ]] &>/dev/null; then
         command git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" "$@"
     else
@@ -11,7 +11,8 @@ function git() {
     fi
 }
 
-function dot() {
+# Alias for git using .dotfiles bare repo that runs status if no arguments provided
+dot() {
     if test "$#" -eq 0; then
         command git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" status
     else
@@ -19,113 +20,23 @@ function dot() {
     fi
 }
 
-# Cleanup local git branches no longer on remote side
-function git_branch_cleanup() {
+# Cleanup local git branches that are no longer found on remote side
+git_branch_cleanup() {
     git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
 }
 
-# Get IP address
-function my_ip()
-{
-    curl ifconfig.co
+# Expand aliases with globalias even when pressing ENTER
+expand-alias-and-accept-line() {
+    globalias
+    zle .backward-delete-char
+    zle .accept-line
 }
-
-# Find a file with a pattern in name:
-function ff()
-{
-    find . -type f -iname '*'"$*"'*' -ls ;
-}
-
-function sys_info()   # Get current host related info.
-{
-    echo -e "\n${BRed}System Informations:$NC " ; uname -a
-    echo -e "\n${BRed}Online User:$NC " ; w -hs | cut -d " " -f1 | sort | uniq
-    echo -e "\n${BRed}Date :$NC " ; date
-    echo -e "\n${BRed}Server stats :$NC " ; uptime
-    echo -e "\n${BRed}Memory stats :$NC " ; free
-    echo -e "\n${BRed}Public IP Address :$NC " ; my_ip
-    echo -e "\n${BRed}Open connections :$NC "; netstat -pan --inet;
-    echo -e "\n${BRed}CPU info :$NC "; cat /proc/cpuinfo ;
-    echo -e "\n"
-}
-
-function extract {
-    if [ -z "$1" ]; then
-        # display usage if no parameters given
-        echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
-    else
-        if [ -f $1 ] ; then
-            NAME=${1%.*}
-            mkdir $NAME && cd $NAME
-            case $1 in
-                *.tar.bz2)   tar xvjf ../$1    ;;
-                *.tar.gz)    tar xvzf ../$1    ;;
-                *.tar.xz)    tar xvJf ../$1    ;;
-                *.lzma)      unlzma ../$1      ;;
-                *.bz2)       bunzip2 ../$1     ;;
-                *.rar)       unrar x -ad ../$1 ;;
-                *.gz)        gunzip ../$1      ;;
-                *.tar)       tar xvf ../$1     ;;
-                *.tbz2)      tar xvjf ../$1    ;;
-                *.tgz)       tar xvzf ../$1    ;;
-                *.zip)       unzip ../$1       ;;
-                *.Z)         uncompress ../$1  ;;
-                *.7z)        7z x ../$1        ;;
-                *.xz)        unxz ../$1        ;;
-                *.exe)       cabextract ../$1  ;;
-                *)           echo "extract: '$1' - unknown archive method" ;;
-            esac
-        else
-            echo "$1 - file does not exist"
-        fi
-    fi
-}
-
-# Creates an archive (*.tar.gz) from given directory.
-function maketar() { tar cvzf "${1%%/}.tar.gz"  "${1%%/}/"; }
-
-# Create a ZIP archive of a file or folder.
-function makezip() { zip -r "${1%%/}.zip" "$1" ; }
-
-function my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,bsdtime,command ; }
-
-function mcd () {
-    mkdir -p $1
-    cd $1
-}
-
-# ex = EXtractor for all kinds of archives - usage: ex <file>
-function ex ()
-{
-    if [ -f $1 ] ; then
-        case $1 in
-            *.tar.bz2)   tar xjf $1   ;;
-            *.tar.gz)    tar xzf $1   ;;
-            *.bz2)       bunzip2 $1   ;;
-            *.rar)       unrar x $1   ;;
-            *.gz)        gunzip $1    ;;
-            *.tar)       tar xf $1    ;;
-            *.tbz2)      tar xjf $1   ;;
-            *.tgz)       tar xzf $1   ;;
-            *.zip)       unzip $1     ;;
-            *.Z)         uncompress $1;;
-            *.7z)        7z x $1      ;;
-            *.deb)       ar x $1      ;;
-            *.tar.xz)    tar xf $1    ;;
-            *)           echo "'$1' cannot be extracted via ex()" ;;
-        esac
-    else
-        echo "'$1' is not a valid file"
-    fi
-}
-
-# Colormap
-function colormap() {
-    for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
-}
+if typeset -f globalias &>/dev/null; then
+    zle -N accept-line expand-alias-and-accept-line
+fi
 
 # Package manager independent update
-function update () {
+update () {
     if [ -e /etc/os-release ] ; then
         source /etc/os-release
         case $ID in
@@ -153,12 +64,46 @@ function update () {
     fi
 }
 
-# A function for expanding any aliases before accepting the line as is and executing the entered command
-expand-alias-and-accept-line() {
-    globalias
-    zle .backward-delete-char
-    zle .accept-line
+# Colormap
+colormap() {
+    for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
-if typeset -f globalias &>/dev/null; then
-    zle -N accept-line expand-alias-and-accept-line
-fi
+
+# Find a file with a pattern in name:
+ff()
+{
+    find . -type f -iname '*'"$*"'*' -ls ;
+}
+
+# Creates an archive (*.tar.gz) from given directory.
+maketar() { tar cvzf "${1%%/}.tar.gz"  "${1%%/}/"; }
+
+# Create a ZIP archive of a file or folder.
+makezip() { zip -r "${1%%/}.zip" "$1" ; }
+
+mcd () {
+    mkdir -p $1
+    cd $1
+}
+
+# Get IP address
+my_ip()
+{
+    curl ifconfig.co
+}
+
+my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,bsdtime,command ; }
+
+# Get current host related info
+sys_info()
+{
+    echo -e "\n${BRed}System Informations:$NC " ; uname -a
+    echo -e "\n${BRed}Online User:$NC " ; w -hs | cut -d " " -f1 | sort | uniq
+    echo -e "\n${BRed}Date :$NC " ; date
+    echo -e "\n${BRed}Server stats :$NC " ; uptime
+    echo -e "\n${BRed}Memory stats :$NC " ; free
+    echo -e "\n${BRed}Public IP Address :$NC " ; my_ip
+    echo -e "\n${BRed}Open connections :$NC "; netstat -pan --inet;
+    echo -e "\n${BRed}CPU info :$NC "; cat /proc/cpuinfo ;
+    echo -e "\n"
+}
