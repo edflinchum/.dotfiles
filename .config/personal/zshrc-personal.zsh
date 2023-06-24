c5e66d6956ade70a3d8e1bade677ac25e5710684
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-24 15:19:31 -0400

    Add additional zsh options

--- .config/personal/zshrc-personal.zsh
+++ .config/personal/zshrc-personal.zsh
@@ -3,15 +3,21 @@
 ######################################################################
 
 # Additional options and completions
-setopt nobeep
-setopt completealiases
-compdef dot=git
+setopt autocd                                                   # if only directory path is entered, cd there
+setopt completealiases                                          # make the alias a distinct command for completion purposes
+setopt extendedglob                                             # extended globbing. Allows using regular expressions with *
+setopt globdots                                                 # don't require a leading . in a filename to be matched
+setopt nocaseglob                                               # case insensitive globbing
+setopt numericglobsort                                          # sort filenames numerically when it makes sense
+setopt rcexpandparam                                            # array expansion with parameters
+setopt appendhistory                                            # immediately append history instead of overwriting
+setopt histignorealldups                                        # if a new command is a duplicate, remove the older one
+setopt correct                                                  # auto correct mistakes
+setopt nocheckjobs                                              # don't warn about running processes when exiting
+setopt nobeep                                                   # no beep
+setopt promptsubst                                              # enable substitution for prompt
 
-# Print some system information when the shell is first started
-if   (( $+commands[fastfetch] )); then fastfetch
-elif (( $+commands[neofetch]  )); then neofetch
-else echo $USER@$HOST  $(uname -srm)  $((( $+commands[lsb_release] )) && lsb_release -rcs)
-fi
+compdef dot=git
 
 # Use micro as default editor
 if (( $+commands[micro] )); then
@@ -19,26 +25,16 @@ if (( $+commands[micro] )); then
   export VISUAL=$EDITOR
 fi
 
-# Do not expand these aliases using omz globalias plugin
-GLOBALIAS_FILTER_VALUES=(
-  cp
-  egrep
-  fgrep
-  gh
-  grep
-  lg
-  ll
-  ls
-  lt
-  mv
-  rm
-  which
-)
-
 # Use 1password cli and plugins
 if (( $+commands[op] )); then
   eval "$(op completion zsh)"; compdef _op op
   if [ -f  ~/.config/op/plugins.sh ]; then
     source ~/.config/op/plugins.sh
   fi
+fi
+
+# Print some system information when the shell is first started
+if   (( $+commands[fastfetch] )); then fastfetch
+elif (( $+commands[neofetch]  )); then neofetch
+else echo $USER@$HOST  $(uname -srm)  $((( $+commands[lsb_release] )) && lsb_release -rcs)
 fi
