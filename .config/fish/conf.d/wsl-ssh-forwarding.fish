f72b1ef2446f2dc74daa19cbfbbf1cebc24df08f
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-06-30 19:41:38 -0400

    Simplify wsl ssh forwarding script

--- .config/fish/conf.d/wsl-ssh-forwarding.fish
+++ .config/fish/conf.d/wsl-ssh-forwarding.fish
@@ -1,13 +1,10 @@
 # Configure ssh forwarding if using WSL
-if string match -qr WSL (uname -a); and type -q socat
+if type -q socat; and string match -qr WSL (uname -a)
     set -gx SSH_AUTH_SOCK $HOME/.ssh/agent.sock
-    if not string match -qr 'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent' (ps -xww)
-        if test -S $SSH_AUTH_SOCK
-            echo "Removing previous socket..."
-            unlink $SSH_AUTH_SOCK
-        end
+    if not ss -lx | grep -q $SSH_AUTH_SOCK
+        rm -f $SSH_AUTH_SOCK
         echo "Starting SSH-Agent relay..."
-        setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork &
+        socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork &>/dev/null &
         disown
     end
 end
--- .config/personal/wsl-ssh-forwarding.zsh
+++ .config/personal/wsl-ssh-forwarding.zsh
@@ -1,12 +1,9 @@
 # Configure ssh forwarding if using WSL
 if (( $+commands[socat] )) && [[ $(uname -a) =~ WSL ]]; then
     export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-    if ! [[ $(ps -xww) =~ 'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent' ]]; then
-        if [ -S $SSH_AUTH_SOCK ]; then
-            echo "Removing previous socket..."
-            unlink $SSH_AUTH_SOCK
-        fi
+    if ! (ss -lx | grep -q $SSH_AUTH_SOCK); then
+        rm -f $SSH_AUTH_SOCK
         echo "Starting SSH-Agent relay..."
-        setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork &
+        setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork &>/dev/null
     fi
 fi
