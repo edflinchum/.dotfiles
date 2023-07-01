188bdb6045d46e55962c1c20c92da2581d1b9f04
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-07-01 18:43:10 -0400

    Fix an issue with wsl ssh script not triggering on arch

--- .config/fish/conf.d/wsl-ssh-forwarding.fish
+++ .config/fish/conf.d/wsl-ssh-forwarding.fish
@@ -1,7 +1,7 @@
 # Configure ssh forwarding if using WSL
 if type -q socat; and string match -qr WSL (uname -a)
     set -gx SSH_AUTH_SOCK $HOME/.ssh/agent.sock
-    if not string match -qr $SSH_AUTH_SOCK (ss -lx)
+    if not string match -qr $SSH_AUTH_SOCK (ps x)
         rm -f $SSH_AUTH_SOCK
         echo "Starting SSH-Agent relay..."
         socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork &>/dev/null &
