ea07888956a0fdcdbd8f029f772273ef0a6e81a2
Ed Flinchum <72924227+DiscFlyer@users.noreply.github.com>
2023-07-01 09:33:07 -0400

    Simplify wsl ssh forwarding script

--- .config/personal/wsl-ssh-forwarding.zsh
+++ .config/personal/wsl-ssh-forwarding.zsh
@@ -1,7 +1,7 @@
 # Configure ssh forwarding if using WSL
 if (( $+commands[socat] )) && [[ $(uname -a) =~ WSL ]]; then
     export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-    if ! (ss -lx | grep -q $SSH_AUTH_SOCK); then
+    if ! [[ $(ss -lx) =~ $SSH_AUTH_SOCK ]]; then
         rm -f $SSH_AUTH_SOCK
         echo "Starting SSH-Agent relay..."
         setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'npiperelay.exe -ei -s //./pipe/openssh-ssh-agent',nofork &>/dev/null
