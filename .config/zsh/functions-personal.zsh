24f2b7812aada34d683a9686a8dbfcbbb350f6f3
Ed Flinchum <72924227+edflinchum@users.noreply.github.com>
2025-05-23 14:13:12 -0400

    Make compatible with bash shell

--- .config/zsh/functions-personal.zsh
+++ .config/zsh/functions-personal.zsh
@@ -4,7 +4,11 @@
 
 # Git wrapper for .dotfiles bare repo when in home folder unless in a different git work tree
 git() {
-    if [[ "$1" != (clone|-C) && "$PWD" =~ "$HOME" && ! $(command git rev-parse --is-inside-work-tree 2>/dev/null) ]]; then
+    if [[ "$1" != "clone"
+       && "$1" != "-C"
+       && "$PWD" =~ "$HOME"
+       && ! $(command git rev-parse --is-inside-work-tree 2>/dev/null) ]]
+    then
         command git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" "$@"
     else
         command git "$@"
