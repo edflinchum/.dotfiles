d6d4ed42dc09183047653c766026dd3b95c79f7c
Ed Flinchum <72924227+edflinchum@users.noreply.github.com>
2025-05-29 08:11:46 -0400

    Improve readability

--- .config/zsh/zshrc-personal.zsh
+++ .config/zsh/zshrc-personal.zsh
@@ -3,19 +3,19 @@
 ######################################################################
 
 # Additional options, completions, and styles
-setopt appendhistory                                            # immediately append history instead of overwriting
-setopt autocd                                                   # if only directory path is entered, cd there
-setopt nobeep                                                   # no beep
-setopt nocaseglob                                               # case insensitive globbing
-setopt nocheckjobs                                              # don't warn about running processes when exiting
-setopt completealiases                                          # make the alias a distinct command for completion purposes
-setopt correct                                                  # auto correct mistakes
-setopt extendedglob                                             # extended globbing. Allows using regular expressions with *
-setopt globdots                                                 # don't require a leading . in a filename to be matched
-setopt histignorealldups                                        # if a new command is a duplicate, remove the older one
-setopt numericglobsort                                          # sort filenames numerically when it makes sense
-setopt rcexpandparam                                            # array expansion with parameters
-setopt promptsubst                                              # enable substitution for prompt
+setopt appendhistory            # immediately append history instead of overwriting
+setopt autocd                   # if only directory path is entered, cd there
+setopt nobeep                   # no beep
+setopt nocaseglob               # case insensitive globbing
+setopt nocheckjobs              # don't warn about running processes when exiting
+setopt completealiases          # make the alias a distinct command for completion purposes
+setopt correct                  # auto correct mistakes
+setopt extendedglob             # extended globbing. Allows using regular expressions with *
+setopt globdots                 # don't require a leading . in a filename to be matched
+setopt histignorealldups        # if a new command is a duplicate, remove the older one
+setopt numericglobsort          # sort filenames numerically when it makes sense
+setopt rcexpandparam            # array expansion with parameters
+setopt promptsubst              # enable substitution for prompt
 
 compdef dot=git
 
